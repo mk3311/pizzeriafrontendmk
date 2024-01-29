84d04b9363fcbfdacd1ee0c181db0fecd43cbe9a
mk3311 <mateuszk3311@gmail.com>
2024-01-29 06:50:19 +0100

    Dodanie

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState,useEffect  } from 'react';
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './index.css';
 import Home from "./pages/Home";
 import Menu from "./pages/PizzaMenu";
